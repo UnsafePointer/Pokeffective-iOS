08fdc2923b2f06ae7d3fd7bd73dd57267bc2adce
Renzo Crisóstomo <ruenzuo@gmail.com>
2014-03-09 13:03:33 +0100

    Fixing PKEListViewController buttons

--- Pokeffective/Controllers/PKEListViewController.m
+++ Pokeffective/Controllers/PKEListViewController.m
@@ -13,6 +13,9 @@
 
 @interface PKEListViewController () <PKETableViewControllerDataSource>
 
+- (void)searchButtonTapped:(id)sender;
+- (void)filterButtonTapped:(id)sender;
+
 @end
 
 @implementation PKEListViewController
@@ -20,11 +23,34 @@
 - (void)viewDidLoad
 {
     [super viewDidLoad];
+    UIBarButtonItem *searchBarButtonItem = [[UIBarButtonItem alloc] initWithImage:[UIImage imageNamed:@"Search"]
+                                                                            style:UIBarButtonItemStyleBordered
+                                                                           target:self
+                                                                           action:@selector(searchButtonTapped:)];
+    UIBarButtonItem *filterBarButtonItem = [[UIBarButtonItem alloc] initWithImage:[UIImage imageNamed:@"Filter"]
+                                                                            style:UIBarButtonItemStyleBordered
+                                                                           target:self
+                                                                           action:@selector(filterButtonTapped:)];
+    [self.navigationItem setRightBarButtonItems:@[searchBarButtonItem, filterBarButtonItem]];
 }
 
 - (void)didReceiveMemoryWarning
 {
     [super didReceiveMemoryWarning];
+}
+
+#pragma mark - Private Methods
+
+- (void)searchButtonTapped:(id)sender
+{
+    [self performSegueWithIdentifier:@"SearchSegue"
+                              sender:self];
+}
+
+- (void)filterButtonTapped:(id)sender
+{
+    [self performSegueWithIdentifier:@"FilterSegue"
+                              sender:self];
 }
 
 #pragma mark - UITableViewDataSource
