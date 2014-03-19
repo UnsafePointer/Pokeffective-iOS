c15fff0aa648c5506646de9a467e925748d66d5b
Renzo Crisóstomo <ruenzuo@gmail.com>
2014-03-19 20:46:35 +0100

    Rounds to cells, fixed

--- Pokeffective/Views/PKETypeCell.m
+++ Pokeffective/Views/PKETypeCell.m
@@ -10,4 +10,13 @@
 
 @implementation PKETypeCell
 
+- (id)initWithCoder:(NSCoder *)aDecoder
+{
+    if ([super initWithCoder:aDecoder]) {
+        [[self layer] setCornerRadius:5.0f];
+        [[self layer] setMasksToBounds:YES];
+    }
+    return self;
+}
+
 @end
